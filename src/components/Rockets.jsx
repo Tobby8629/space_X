8d533e83616ec7b399a4520b464379df86a7f188
tobby8629 <sarhmullah15@gmail.com>
2023-03-04 19:03:50 -0800

    display rockets

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import { RocketApi } from '../redux/Rockets/RocketSlice';
 
 function Rockets() {
+    const dispatch = useDispatch();
+    const rocket = useSelector((state)=>state.Rocket.rockets)
+useEffect(() => {
+  dispatch((RocketApi()))
+},[dispatch])
+console.log(rocket)
   return (
-    <div>Rockets</div>
+    <div>
+      {rocket.map((rocket)=>(
+        <div className="each" key={rocket.id}>
+          <div className="image">
+            <img src={rocket.images[0]} alt="" />
+          </div>
+          <div className="text">
+           <h3>{rocket.rocket_name}</h3>
+           <p>
+            {rocket.reserved ? <span className='reserve'> reserved </span> : null}
+            {rocket.description}
+           </p>
+           <button>
+            {rocket.reserved ? "Cancel Reservation" : "reserve rocket"}
+           </button>
+          </div>
+
+        </div>
+      ))}
+    </div>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import missionSlice from "./Mission/missionSlice"
+import RocketSlice from "./Rockets/RocketSlice";
 const store = configureStore({
     reducer: {
       Mission: missionSlice,
+      Rocket: RocketSlice
     }
 })
 
